f7b679108ce18bae34c2c2dd8cf4d2807f2e20aa
Rafaela Vasconcellos <rafaela@oktana.io>
2017-09-27 16:35:09 -0300

    Initial commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,12 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import Pusher from 'pusher-js';
+
+Pusher.logToConsole = true;
+const socket = new Pusher('d2c236fdf98a911edb8a', {
+  cluster: 'us2',
+  encrypted: true
+});
 
 @Component({
   selector: 'page-home',
@@ -8,7 +15,10 @@ import { NavController } from 'ionic-angular';
 export class HomePage {
 
   constructor(public navCtrl: NavController) {
-
+    const channel = socket.subscribe('my-channel');
+    channel.bind('my-event', function(data) {
+      alert(data.message);
+    });
   }
 
 }
